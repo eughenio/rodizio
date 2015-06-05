676961bbe4e8a65e1d8c70ee1f5bfeb162d4c6b2
Eughenio Constantino <eughenio@gmail.com>
2015-06-04 23:59:51 -0300

    migrations created

---
+++ database/migrations/2015_06_05_024439_CreatePlatesTable.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlatesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('plates', function(Blueprint $table)
+        {
+            $table->increments('id_plate')->index();
+            $table->integer('end');
+            $table->integer('fk_user')->unsigned();
+            $table->boolean('receive_email');
+            $table->timestamps();
+            $table->foreign('fk_user')->references('id_user')->on('users');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('plates');
+	}
+
+}
